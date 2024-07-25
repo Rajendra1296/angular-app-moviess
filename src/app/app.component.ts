3a3b90decfadb5ca8c615df36964f0f4d68de077
Rajendra1296 <rajendra.venigalla@proclink.com>
2024-07-25 11:41:36 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MoviedataComponent } from './user/user.component.spec';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [MoviedataComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'my-first-project';
+  // name = 'Monkey D Luffy🏴‍☠️🏴‍☠️🏴‍☠️';
+  // url =
+  //   'https://image.winudf.com/v2/image/Y29tLmJhbGVmb290Lk1vbmtleURMdWZmeVdhbGxwYXBlcl9zY3JlZW5fMF8xNTI0NTE5MTEwXzAyOA/screen-0.jpg?fakeurl=1&type=.jpg';
 }
